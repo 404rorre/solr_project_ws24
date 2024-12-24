dc20fcc7597ce06a36553a053e99e3aadc5d67a7
404rorre <105109034+404rorre@users.noreply.github.com>
2024-12-25 00:44:18 +0100

    added versioning in run_batch.py

--- scripts/run_batch.py
+++ scripts/run_batch.py
@@ -3,24 +3,26 @@ from pysearch import QUERY
 
 
 cores = [
-    "base_bm25"
+    "base_LMDirichlet"
 ]
 queries= [
     "title:(query)",
     "abstract:(query)",
     "title:(query) abstract:(query)"
 ]
+version_start= 1
 versions = [
     n for n in range(1, len(queries)+1)
 ]
+
 
 n = 0
 for core in cores:
     with tqdm(total=len(cores)*len(versions), desc="Running Settings", position=0) as pbar:
         for version in versions:
             query = queries[version-1]
-            print("Running...", f"Core:{core}", f"Version:{version}", f"Query:{query}", sep="\t")
-            solr = QUERY(version=str(version), 
+            print("Running...", f"Core:{core}", f"Version:{version+version_start-1}", f"Query:{query}", sep="\t")
+            solr = QUERY(version=str(version+version_start-1), 
                         core=core, 
                         rows=1000, 
                         url_query=query)
