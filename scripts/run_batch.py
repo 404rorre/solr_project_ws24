90c792956f059086f1262dd5dc30d1f447f27a70
404rorre <404rorre.edoc@gmail.com>
2025-01-04 01:03:38 +0100

    Added Runs query with bool OR & Added Runs with simple field boosting

--- scripts/run_batch.py
+++ scripts/run_batch.py
@@ -13,15 +13,15 @@ cores = [
 queries= [
     #"title:($11)",
     #"abstract:($11)",
-    #"title:($11) abstract:($11)"
-    "title:(query) OR abstract:(query)"
+    "title:($11)^5 AND abstract:($11)^2",
+    "title:($11)^5 OR abstract:($11)^2"
 ]
-version_start= 13
+version_start= 24
 versions = [
     n for n in range(1, len(queries)+1)
 ]
-#df_topics =  pd.read_csv("data/topics/topics_cluster_expansion.csv")
-topic_file = "data/topics/topics-rnd5.xml"
+df_topics =  pd.read_csv("topic_expansions/topics_cluster_expansion.csv")
+#topic_file = "data/topics/topics-rnd5.xml"
 n = 0
 for core in cores:
     #with tqdm(total=len(cores)*len(versions), desc="Running Settings", position=-0) as pbar: # maybe later too much time for debugging
@@ -33,8 +33,8 @@ for core in cores:
                     core=core, 
                     rows=1000, 
                     url_query=query,
-                    #df_topics=df_topics,
-                    topicfile=topic_file
+                    df_topics=df_topics,
+                    #topicfile=topic_file
                 )        
 
         solr.run()
