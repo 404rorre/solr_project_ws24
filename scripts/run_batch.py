8fa38931abdc383370edd42661a12c310b1eec1f
404rorre <404rorre.edoc@gmail.com>
2025-01-04 14:56:51 +0100

    Added Field Boosting Runs

--- scripts/run_batch.py
+++ scripts/run_batch.py
@@ -5,47 +5,50 @@ from pysearch import QUERY
 
 
 cores = [
-    "textEN_bm25",
-    "textEN_DFI",
-    "textEN_IBS",
-    "textEN_LMDirichlet"    
+    "textEN_bm25",  
 ]
 boosting = [
                 (2, 1),
-                (5, 2),
                 (1, 2),
                 (2, 5) # TODO do tomorrow
             ]
-queries= [
-    #"title:($11)",
-    #"abstract:($11)",
-    #"title:($11)^5 AND abstract:($11)^2",
-    "title:($11)^5 OR abstract:($11)^2"
-]
-version_start= 24
-versions = [
-    n for n in range(1, len(queries)+1)
-]
-df_topics =  pd.read_csv("topic_expansions/topics_cluster_expansion.csv")
-#topic_file = "data/topics/topics-rnd5.xml"
-n = 0
-for core in cores:
-    #with tqdm(total=len(cores)*len(versions), desc="Running Settings", position=-0) as pbar: # maybe later too much time for debugging
-    for version in versions:
-        query = queries[version-1]
-        print("Running...", f"Core:{core}", f"Version:{version+version_start-1}", f"Query:{query}", sep="\t")
-        solr = QUERY(
-                    version=str(version+version_start-1), 
-                    core=core, 
-                    rows=1000, 
-                    url_query=query,
-                    df_topics=df_topics,
-                    #topicfile=topic_file
-                )        
 
-        solr.run()
-        #pbar.update(n)
-        n += 1
+version_start= 44
+
+
+for boost in boosting:
+    queries= [
+        #"title:($11)",
+        #"abstract:($11)",
+        f"title:($11)^{boost[0]} AND abstract:($11)^{boost[1]}",
+        f"title:($11)^{boost[0]} OR abstract:($11)^{boost[1]}"
+    ]
+    
+    
+    versions = [
+    n for n in range(1, len(queries)+1)
+    ]
+    df_topics =  pd.read_csv("topic_expansions/topics_cluster_expansion.csv")
+    #topic_file = "data/topics/topics-rnd5.xml"
+    n = 0
+    for core in cores:
+        #with tqdm(total=len(cores)*len(versions), desc="Running Settings", position=-0) as pbar: # maybe later too much time for debugging
+        for version in versions:
+            query = queries[version-1]
+            print("Running...", f"Core:{core}", f"Version:{version+version_start-1}", f"Query:{query}", sep="\t")
+            solr = QUERY(
+                        version=str(version+version_start-1), 
+                        core=core, 
+                        rows=1000, 
+                        url_query=query,
+                        df_topics=df_topics,
+                        #topicfile=topic_file
+                    )        
+
+            solr.run()
+            #pbar.update(n)
+            n += 1
+    version_start += len(versions)
 
 
 print("Finished runs")
