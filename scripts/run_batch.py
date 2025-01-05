d16abf7c6988716a719f5fe57ea51afb5cdf6a11
404rorre <404rorre.edoc@gmail.com>
2025-01-05 20:05:38 +0100

    Added more files from schema & Fixed Copying Schema subfolders on core installation

--- scripts/run_batch.py
+++ scripts/run_batch.py
@@ -5,30 +5,36 @@ from pysearch import QUERY
 
 
 cores = [
-    "textEN_bm25_nltkstopw",  
+    "textEN_bm25", 
 ]
 #boosting = [2, 3, 4, 5]
 
-version_start= 1
+version_start= 10001
 
 
 #for boost in boosting:
+
+
 queries= [
-    "title:($6)",
-    #"abstract:(query)",
-    #"title:(query) AND abstract:(query)",
-    #"title:(query) OR abstract:(query)",
+    "title:(query)",
+    "abstract:(query)",
+    "title:(query) AND abstract:(query)",
+    "title:(query) OR abstract:(query)",
     
     #f"title:(query)^1 AND abstract:(query)^{boost}",
     #f"title:(query)^1 OR abstract:(query)^{boost}"
 ]
 
 
+# df_topics =  pd.read_csv("topic_expansions/topics_llm_queryexpansion.csv")
+topic_file = "data/topics/topics-rnd5.xml"
+
+##################################################################################################
+##################################################################################################
 versions = [
 n for n in range(1, len(queries)+1)
 ]
-df_topics =  pd.read_csv("topic_expansions/topics_llm_queryexpansion.csv")
-#topic_file = "data/topics/topics-rnd5.xml"
+
 n = 0
 for core in cores:
     #with tqdm(total=len(cores)*len(versions), desc="Running Settings", position=-0) as pbar: # maybe later too much time for debugging
@@ -40,8 +46,8 @@ for core in cores:
                     core=core, 
                     rows=1000, 
                     url_query=query,
-                    df_topics=df_topics,
-                    #topicfile=topic_file
+                    #df_topics=df_topics,
+                    topicfile=topic_file
                 )        
 
         solr.run()
