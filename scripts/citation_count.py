6b1fc3d395059f64cd994d30b191f78a5c394ab9
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-01-08 02:04:25 +0100

    minor adjustments to citation count script

--- scripts/citation_count.py
+++ scripts/citation_count.py
@@ -14,9 +14,9 @@ def get_citationCount_by_paperID(paperID: str) -> int | None:
 
 def get_citationCount_by_DOI(DOI: str) -> int | None: 
     url = f"https://api.semanticscholar.org/graph/v1/paper/DOI:{DOI.strip()}?fields=citationCount"
+    response: requests.Response = requests.get(url)
     while True: 
         try: 
-            response: requests.Response = requests.get(url)
             if response.status_code == 429: 
                 print("too many requests, asking again...")
                 continue
@@ -24,7 +24,10 @@ def get_citationCount_by_DOI(DOI: str) -> int | None:
                 raise Exception()
             return int(response.json()["citationCount"])
         except: 
+            if "not found" in response.json()["error"]: 
+                return None
             print(f"{response.json() = }")
+            print(f"{response.status_code = }")
             return None
 
 
